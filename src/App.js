bd98c336a60721a1b5bbe3070fa758f9c258db9c
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-09-29 11:44:55 +0530

    useRef hook part2

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import DataFetching1 from './components/DataFetching1';
 import DataFetching2 from './components/DataFetching2';
 import FocusInput from './components/FocusInput';
+import HookTimer from './components/HookTimer';
 
 function App() {
   return (
     <div className="App">
       {/* <DataFetching1 /> */}
       {/* <DataFetching2 /> */}
-      <FocusInput />
+      {/* <FocusInput /> */}
+      <HookTimer />
     </div>
   );
 }
