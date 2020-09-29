ad91f4ed0d486d041e2757d83c862794a3ebafc4
Ghanashyam Kamath <ghanashyam.kamath99@gmail.com>
2020-09-29 11:10:53 +0530

    fetching using useReducer

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DataFetching1 from './components/DataFetching1';
+
+function App() {
+  return (
+    <div className="App">
+      <DataFetching1 />
+    </div>
+  );
+}
+
+export default App;
